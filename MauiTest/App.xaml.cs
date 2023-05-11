a4ee462df9b9a17a763b1565f48514d04d21b4e8
Pabloxxa <distribuidaitm@gmail.com>
2023-05-11 16:16:02 -0500

    Tabbed Page

--- MauiTest/App.xaml.cs
+++ MauiTest/App.xaml.cs
@@ -5,12 +5,24 @@
         public App()
         {
             InitializeComponent();
-
+            #region
+            //1. Content Page Demo
             //var navPage = new NavigationPage(new ContentPageDemo());
             //navPage.BarBackgroundColor = Colors.Chocolate;
             //navPage.BarTextColor = Colors.White;
+            #endregion   
+            #region
+            //2. Flyout Demo Page
+            //MainPage = new NavigationPage(new FlyoutPageDemo());
+            #endregion       
+            #region
+            //3. TabbedPageDemo
+            #endregion
 
-            MainPage = new NavigationPage(new FlyoutPageDemo());
+            var navPage = new NavigationPage(new TabbedPageDemo());
+            //navPage.BarBackgroundColor = Colors.Chocolate;
+            //navPage.BarTextColor = Colors.White;
+            MainPage = navPage;
         }
     }
 }
