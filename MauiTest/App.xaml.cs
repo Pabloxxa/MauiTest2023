e1ecdf234e8db6435b2009433ec511eca8134ff1
Pabloxxa <distribuidaitm@gmail.com>
2023-05-11 12:31:50 -0500

    FlyoutPage

--- MauiTest/App.xaml.cs
+++ MauiTest/App.xaml.cs
@@ -6,11 +6,11 @@
         {
             InitializeComponent();
 
-            var navPage = new NavigationPage(new ContentPageDemo());
-            navPage.BarBackgroundColor = Colors.Chocolate;
-            navPage.BarTextColor = Colors.White;
-            MainPage = navPage;
+            //var navPage = new NavigationPage(new ContentPageDemo());
+            //navPage.BarBackgroundColor = Colors.Chocolate;
+            //navPage.BarTextColor = Colors.White;
 
+            MainPage = new NavigationPage(new FlyoutPageDemo());
         }
     }
 }
---
+++ MauiTest/FlyoutPageDemo.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiTest;
+
+public partial class FlyoutPageDemo : FlyoutPage
+{
+	public FlyoutPageDemo()
+	{
+		InitializeComponent();
+	}
+}
